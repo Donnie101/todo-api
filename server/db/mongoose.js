be21ba478957b8e2c64caba7d1ba95c67678d3b6
L <L>
2017-07-03 20:03:53 +0100

    env var db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 var db = process.env.MONGODB || 'mongodb://localhost:27017/TodoApp';
-mongoose.connect(MONGODB);
+mongoose.connect(db);
 
 //mongodb://localhost:27017/TodoApp
 module.exports = {mongoose};
