5498cf6e10575bb86a4c40eb6865aeb61048ba2f
L <L>
2017-07-05 12:04:22 +0100

    add env port

--- server/server.js
+++ server/server.js
@@ -1,3 +1,16 @@
+var env = process.env.NODE_ENV || 'development';
+
+if(env === 'development'){
+  process.env.PORT = 3000;
+  process.env.MONGODB = 'mongodb://localhost:27017/TodoApp';
+}else if (env === 'test') {
+  process.env.PORT = 3000;
+  process.env.MONGODB = 'mongodb://localhost:27017/TodoAppTest';
+}
+/*export NODE_ENV=test ||*/
+//SET \"NODE_ENV=test\" && 
+console.log(  process.env.MONGODB +'***************');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const _ = require('lodash');
